5ffe40bfe9f0005c55055862902ac2e4101c9814
dilip <dilip.n@ideas2it.com>
2022-12-15 19:04:39 +0530

    address CRUD completed

--- src/main/java/com/ideas2it/fooddeliverymanagement/service/AddressService.java
+++ src/main/java/com/ideas2it/fooddeliverymanagement/service/AddressService.java
@@ -1,17 +1,20 @@
 package com.ideas2it.fooddeliverymanagement.service;
 
 import com.ideas2it.fooddeliverymanagement.dto.AddressDTO;
-import com.ideas2it.fooddeliverymanagement.dto.UserDTO;
+import com.ideas2it.fooddeliverymanagement.exception.FoodDeliveryManagementException;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AddressService {
 
-    Optional<AddressDTO> addAddress(AddressDTO addressDTO, int userId);
+    Optional<AddressDTO> addAddress(AddressDTO addressDTO, int userId) throws FoodDeliveryManagementException;
 
-    Optional<AddressDTO> getAddress(int AddressId);
+    AddressDTO getAddress(int AddressId)throws FoodDeliveryManagementException;
 
-    void update(AddressDTO addressDTO);
+    Optional<AddressDTO> updateAddress(int userId,AddressDTO addressDTO) throws FoodDeliveryManagementException;
 
-    Optional<AddressDTO> deleteAddress(int userID, int addressId);
+    Optional<AddressDTO> deleteAddress(int userID, int addressId) throws FoodDeliveryManagementException;
+
+    List<AddressDTO> getAllAddress() throws FoodDeliveryManagementException ;
 }
